8d377d95b30f6f79da0a016e4a9e4bd8f0f262e5
ashish bisht <ashishbisht723@gmail.com>
2019-12-09 23:25:18 +0530

    print branch

---
+++ binary_trees/print_branch.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def tree_paths(node):
+    traverse(node, [])
+
+
+def traverse(node, path):
+    if not node:
+        return
+
+    path.append(node.val)
+
+    if node.left is None and node.right is None:
+        print(path)
+
+    traverse(node.left, path)
+    traverse(node.right, path)
+    # path.pop()
+
+
+if __name__ == "__main__":
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+
+    print(tree_paths(root))

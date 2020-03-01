03393434c94ac299a26639b6fa8cf9a6d81cd552
ashish bisht <ashishbisht723@gmail.com>
2020-03-01 12:34:07 +0530

    longest increasing subseq

---
+++ dp/LongestIncreasingSubsequence.py
@@ -0,0 +1,13 @@
+def longest_increasing_subsequence(nums):
+    dp = [1] * len(nums)
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+
+    return max(dp)
+
+
+nums = [10, 9, 2, 5, 3, 4]
+
+print(longest_increasing_subsequence(nums))
